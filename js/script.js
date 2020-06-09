33c28af107ab167fc48e9422c9cddabed0ca6c92
Tupal-cell <chris.quev@gmail.com>
2020-06-09 16:44:05 +0200

    formular

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-$(function() {
-	var header = $('.noBackground');
-	$(window).scroll(function() {
-		var scroll = $(window).scrollTop();
-
-		if (scroll >= 500) {
-			header.removeClass('noBackground').fadeIn();
-		} else {
-			header.fadeOut().addClass('noBackground');
-		}
-	});
-});
-
 $(function(){
 
     $(".navbar a, footer a").on("click", function(event){
@@ -18,11 +5,40 @@ $(function(){
         event.preventDefault();
         var hash = this.hash;
         
-        $('body,html').animate({scrollTop: $(hash).offset().top} , 900 , function(){window.location.hash = hash;})
+        $('body,html').animate({scrollTop: $(hash).offset().top} , 800 , function(){window.location.hash = hash;})
         
+	});
+	
+	
+    $('#contact-form').submit(function(e) {
+        e.preventDefault();
+        $('.comments').empty();
+        var postdata = $('#contact-form').serialize();
+        
+        $.ajax({
+            type: 'POST',
+            url: 'php/contact.php',
+            data: postdata,
+            dataType: 'json',
+            success: function(json) {
+                 
+                if(json.isSuccess) 
+                {
+                    $('#contact-form').append("<p class='thank-you'>Votre message a bien été envoyé. Merci de m'avoir contacté :)</p>");
+                    $('#contact-form')[0].reset();
+                }
+                else
+                {
+                    $('#firstname + .comments').html(json.firstnameError);
+                    $('#name + .comments').html(json.nameError);
+                    $('#email + .comments').html(json.emailError);
+                    $('#phone + .comments').html(json.phoneError);
+                    $('#message + .comments').html(json.messageError);
+                }                
+            }
+        });
     });
 
 })
-
 
     
